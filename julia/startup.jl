f65faa3e3905a82edde549e9cffd6b5821b96137
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2020-12-04 14:50:46 +0200

    add advent of code cookie for julia package

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,10 +1,13 @@
+# ln -s /home/moorits/dotfiles/julia/startup.jl /home/moorits/.julia/config/startup.jl
+
 function initcommands(repl)
 
-@eval ENV["JULIA_EDITOR"] = "micro"
+@eval ENV["AOC_SESSION"] = "53616c7465645f5fc6bc0f8e55a320e50ba92f5c182daf6037bafc0b79038e95f663b816ff7135f61e0c0f2c6e5f354c"
+@eval ENV["JULIA_EDITOR"] = "nano"
 
 # packages
 @eval using OhMyREPL
-@eval using Revise
+# @eval using Revise
 
 end
 
